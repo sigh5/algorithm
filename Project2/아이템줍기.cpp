8076728f01ddfc37ca2e2c5a6f327f8dba6c9a05
HyunWook <sigh2879@gmail.com>
2023-08-16 14:04:18 +0900

    Update 아이템줍기.cpp

--- Project2/아이템줍기.cpp
+++ Project2/아이템줍기.cpp
@@ -5,12 +5,41 @@
 
 using namespace std;
 
-int world[52][52];
+int world[101][101];
+int answer = 100'000'000;
+
+int xDir[4] = { -1,1,0,0 };
+int yDir[4] = { 0,0,-1,1 };
+
+void dfs(int startX, int startY, int goalX, int goalY, int count)
+{
+    if (startX == goalX && startY == goalY)
+    {
+        answer = min(count, answer);
+        return;
+    }
+
+    for (int i = 0; i < 4; ++i)
+    {
+        int NewX = xDir[i] + startX;
+        int NewY = yDir[i] + startY;
+
+
+        if (NewY >= 0 && NewY <= 100 &&
+            NewX >= 0 && NewX <= 100 &&
+            world[NewX][NewY] == 7)
+        {
+            world[NewX][NewY] = 0;
+            dfs(NewX, NewY, goalX, goalY, count + 1);
+            world[NewX][NewY] = 7;
+        }
+    }
+
+}
 
 
 int solution(vector<vector<int>> rectangle, int characterX, int characterY, int itemX, int itemY)
 {
-    int answer = 0;
     //1. 도형이 겹쳤을 때 일단 바깥쪽 테투리를 따는법 
     //
     // 1. 일단 도형이 들어오면 전체 너비를 1로 채운다
@@ -18,13 +47,19 @@ int solution(vector<vector<int>> rectangle, int characterX, int characterY, int 
 
     int rectangleSize = rectangle.size();
 
+    characterX *= 2, characterY *= 2, itemX *= 2, itemY *= 2;
+
+
+
+
+
     for (int i = 0; i < rectangleSize; ++i)
     {
-        for (int j = rectangle[i][0]; j < rectangle[i][2]; ++j)
+        for (int j = rectangle[i][0] *2 ; j <= rectangle[i][2] *2; ++j)
         {
-            for (int q = rectangle[i][1]; q < rectangle[i][3]; ++q)
+            for (int q = rectangle[i][1]*2; q <= rectangle[i][3]*2; ++q)
             {
-                world[j][q] += 1;
+                world[j][q] = 7;
             }
         }
     }
@@ -32,29 +67,21 @@ int solution(vector<vector<int>> rectangle, int characterX, int characterY, int 
 
     for (int i = 0; i < rectangleSize; ++i)
     {
-        for (int j = rectangle[i][0]; j < rectangle[i][2]; ++j)
+        for (int j = rectangle[i][0]* 2 +1; j <= rectangle[i][2] *2 -1; ++j)
         {
-            for (int q = rectangle[i][1]; q < rectangle[i][3]; ++q)
+            for (int q = rectangle[i][1]*2+1; q <= rectangle[i][3]*2-1; ++q)
             {
-                if (world[j][q] >= 2)
+                
                     world[j][q] = 0;
             }
         }
     }
 
   
-    for (int i = 0; i < 10; i++)
-    {
-        for (int j = 0; j < 10; ++j)
-        {
-            cout << world[i][j] << " ";
-        }
-        puts("\n");
-    }
+    world[characterX][characterY] = 1;
+    dfs(characterX, characterY, itemX, itemY, 0);
 
-
-
-    return answer;
+    return static_cast<int>(answer/2);
 }
 
 
@@ -62,7 +89,7 @@ int main()
 {
     vector<vector<int>> rectangle{ {1,1,7,4} ,{3,2,5,5},{4,3,6,9},{2,6,8,8} };
 
-    solution(rectangle, 1, 3, 7, 8);
+    cout << solution(rectangle, 1, 3, 7, 8);
 
 
     return 0;
